55dd4f9335b979979e1c7910278a2d674ea0d636
kouta <kouta@kouta.kouta>
2020-05-11 00:17:27 +0900

    turai

---
+++ html/css/forIE.js
@@ -0,0 +1,2 @@
+h5=new Array('abbr', 'article', 'section', 'nav', 'aside', 'figure', 'figcaption', 'header', 'footer', 'main', 'mark', 'time', 'bdi', 'details', 'summary', 'video', 'audio', 'source', 'track', 'progress', 'meter', 'datalist', 'command', 'keygen', 'canvas');
+i=h5.length; while (--i>=0) document.createElement(h5[i]);
